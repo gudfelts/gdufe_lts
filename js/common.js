34fe3496c3a228329a3a6b99737da55b94c32c63
Roxk-33 <xiaorock@126.com>
2017-10-07 00:46:00 +0800

    修改导航栏bug

--- js/common.js
+++ js/common.js
@@ -1,42 +1,17 @@
 function showDiv(obj) {
-    obj.show(0).css('opacity',1);
-  }
-  /**
-   * 
-   * @param {*} obj 对象
-   * @param {*} isDelay 是否延迟隐藏 
-   */
-  function hideDiv(obj, isDelay) {
-    if (isDelay) {
-      obj.delay(500).hide(0).css('opacity',0);
-      
-    } else {
-      obj.hide(0).css('opacity',0);
-    }
-  
-    
-  }
-  
-  $().ready(function(e) {
-    /*轮播图控制*/
-   
-  
-    //动态调整rem
-    // document.documentElement.style.fontSize =
-    //   document.documentElement.clientWidth / 32 + "px";
-  
-    //导航栏控制
-  
-    $(".nav_item").mouseenter(function() {
-      $(".item_content").hide(0);
-      showDiv($(this).children(".item_content"));
-    });
-    $(".nav_item").mouseleave(function() {
-      hideDiv($(this).children(".item_content"), true);
-    });
-  
-    $(".item_content").mouseleave(function() {
-      hideDiv($(this), false);
-    });
+  obj.show(0).css("display", "block");
+}
+
+function hideDiv(obj) {
+  obj.hide(0).css("display", "none");
+}
+
+$().ready(function(e) {
+  $(".nav_item").mouseenter(function() {
+    $(".item_content").hide(0);
+    showDiv($(this).children(".item_content"));
   });
-  
+  $(".nav_item").mouseleave(function() {
+    hideDiv($(this).children(".item_content"), true);
+  });
+});
