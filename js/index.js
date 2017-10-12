9d157e713cf96b7af28d19d2aa4128cc7456c908
Roxk-33 <xiaorock@126.com>
2017-10-12 11:23:07 +0800

    修改标题、更换轮播图插件

--- js/index.js
+++ js/index.js
@@ -2,17 +2,13 @@
 
 $().ready(function(e) {
   /*轮播图控制*/
-  var unslider04 = $('.banner').unslider({
-          dots: true,
-          fluid: true  
-      }),
-      data04 = unslider04.data('unslider');
-
-  $('.unslider-arrow04').click(function () {
-      var fn = this.className.split(' ')[1];
-      data04[fn]();
-  });
-
+  $('.banner').slick({
+    dots: true,
+    autoplay : true,
+    arrows : true,
+    fade: true,
+    cssEase: 'linear'
+});
   
 
 
